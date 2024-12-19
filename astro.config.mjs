379a39bfed6cdbe84a4446630714227f5b1eb69d
Ryan Diaz <rd37011@gmail.com>
2024-12-19 18:18:04 -0500

    add Preact component, add light / dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://rd37011.github.io"
+  site: "https://rd37011.github.io",
+  integrations: [preact()]
 });
